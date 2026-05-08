00d93071f5de5cc508c852e42596e044965b69bb
Cameron <swanc@me.com>
2026-05-07 23:39:42 -0300

    Added rotating file logging.

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,35 +1,66 @@
-const winston = require('winston');
+const winston = require('winston')
+const fs = require('fs')
+const path = require('path')
+const { info } = require('console')
+require('winston-daily-rotate-file')
 
 const createLogger = (source) => {
+
+    const baseFormat = winston.format.combine(
+        winston.format.label({ label: source}),
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+        winston.format.errors({ stack: true }),
+        winston.format.printf(({ timestamp, label, level, message, stack, ...meta }) => {
+
+            let logString = `[${timestamp}]`
+            logString += ` [${level}]`
+            logString += ` [${label}]`.padEnd(12, ' ')
+
+            if (meta.type) logString += `[${meta.type}]`.padEnd(14, ' ')
+            if (meta.channel) logString += `Channel: ${meta.channel} | `
+            if (meta.user) logString += `User: ${meta.user}`
+            if (meta.user && (meta.command || message)) logString += ' | '
+            if (meta.command) logString += `Command: /${meta.command}`
+            if (meta.command && message) logString += ' | '
+            if (message) logString += `${message}`
+
+            return logString
+        })
+    )
+
+    const consoleTransport = new winston.transports.Console({
+        level: process.env.CONSOLE_LOG_LEVEL || 'debug',
+        format: winston.format.combine(
+            winston.format.colorize(),
+            baseFormat,
+        )
+    })
+
+    const logDirectory = process.env.LOG_DIR_NAME || 'logs'
+
+    if (!fs.existsSync(logDirectory)) {
+        fs.mkdirSync(logDirectory, { recursive: true })
+    }
+
+    const logFile = path.join(logDirectory, `%DATE%-${process.env.LOG_FILE_NAME || '%DATE%-league-bot.log'}`)
+
+    const fileRotateTransport = new winston.transports.DailyRotateFile({
+        level: process.env.FILE_LOG_LEVEL || 'info',
+        filename: logFile,
+        maxSize: process.env.MAX_LOG_SIZE || '7m',
+        maxFiles: parseInt(process.env.MAX_LOG_FILES || '10'),
+        zippedArchive: true,
+        datePattern: 'YYYY-MM-DD',
+        format: baseFormat,
+    })
+
     return winston.createLogger({
         level: 'debug',
-        format: winston.format.combine(
-            winston.format.label({ label: source}),
-            winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
-            winston.format.errors({ stack: true }),
-            winston.format.colorize(),
-            winston.format.printf(({ timestamp, label, level, message, stack, ...meta }) => {
-
-                let logString = `[${timestamp}]`
-                logString = logString += ` [${level}]`
-                logString = logString += ` [${label}]`.padEnd(12, ' ')
-
-                if (meta.type) logString += `[${meta.type}]`.padEnd(14, ' ')
-                else if (meta.channel && meta.type === 'VOICE') logString += 'Voice '
-                if (meta.channel) logString += `Channel: ${meta.channel} | `
-                if (meta.user) logString += `User: ${meta.user}`
-                if (meta.user && (meta.command || message)) logString += ' | '
-                if (meta.command) logString += `Command: /${meta.command}`
-                if (meta.command && message) logString += ' | '
-                if (message) logString += `${message}`
-
-                return logString
-            })
-        ),
         transports: [
-            new winston.transports.Console()
+            consoleTransport,
+            fileRotateTransport,
         ]
     });
 }
 
-module.exports = createLogger;
+module.exports = createLogger
